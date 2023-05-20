3fa69f3a5ea270fd1d2c064f71bf2895a2ff90a1
Stijn Leenknegt <stijn@diagro.be>
2023-05-20 23:00:28 +0200

    testing

--- jeebby/status.js
+++ jeebby/status.js
@@ -1,17 +1,9 @@
 module.exports = function(RED) {
     function JeebbyStatus(config) {
         RED.nodes.createNode(this,config);
-
-        const nodeContext = this.context();
-        console.log(nodeContext);
-        console.log(RED);
-
-    //     var flowContext = this.context().flow;
-
-    // var globalContext = this.context().global;
-    //     this.name = config.name;
-    //     this.id = config.id;
+        const teamId = this.context().global.jeebby.teamId; //staat in /data/settings.js # functionGlobalContext 
+        console.log('TeamId: ' + teamId);
     }
 	
-    RED.nodes.registerType("status",JeebbyStatus);
+    RED.nodes.registerType("jeebby-status",JeebbyStatus);
 }
